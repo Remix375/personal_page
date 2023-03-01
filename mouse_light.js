78c2c9ac1a1ca9aa317e31ad5c5552f47358f01a
Remix375 <bigbossremi@gmail.com>
2023-03-01 18:14:22 +0100

    border change color

--- mouse_light.js
+++ mouse_light.js
@@ -3,7 +3,7 @@ const light = document.getElementById("light");
 
 
 document.addEventListener('mousemove', function(x) {
-
     light.animate({top: `${x.clientY}px`, left: `${x.clientX}px`}, {duration: 1000, fill: "forwards"})
+    light.animate({opacity: "1"}, {duration: 4000, fill: "forwards"})
 });
 
