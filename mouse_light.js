e1c9c72b9eb04c5677de9657bb5414c5483f559c
Remix375 <bigbossremi@gmail.com>
2023-03-03 13:43:37 +0100

    new nav bar

--- mouse_light.js
+++ mouse_light.js
@@ -1,9 +1,27 @@
 
 const light = document.getElementById("light");
+const cursor = document.getElementById("cursor");
 
+let first = true;
 
 document.addEventListener('mousemove', function(x) {
-    light.animate({top: `${x.clientY}px`, left: `${x.clientX}px`}, {duration: 1000, fill: "forwards"})
-    light.animate({opacity: "1"}, {duration: 4000, fill: "forwards"})
+    if (first) {
+        light.style.top = `${x.clientY}px`;
+        light.style.left = `${x.clientX}px`;
+
+        cursor.style.top = `${x.clientY}px`;
+        cursor.style.left = `${x.clientX}px`;
+
+        light.animate({opacity: "1"}, {duration: 2000, fill: "forwards"});
+        cursor.animate({opacity: "1"}, {duration: 1000, fill: "forwards"})
+        first = false;
+    }
+    else {
+        light.animate({top: `${x.clientY}px`, left: `${x.clientX}px`}, {duration: 1000, fill: "forwards"});
+        cursor.animate({top: `${x.clientY}px`, left: `${x.clientX}px`}, {duration: 0, fill: "forwards"});
+    }
+
 });
+
+
 
