baa6060c5be95e619981ecb720305430c76cb5ed
Remix375 <bigbossremi@gmail.com>
2023-03-04 16:45:30 +0100

    change language

---
+++ language.js
@@ -0,0 +1,13 @@
+const current_lang = "English"
+
+
+
+function change_lang(language) {
+    if (language === current_lang) {
+        return;
+    }
+    console.log(`language changing to ${language}`);
+    console.log(".");
+    console.log("..");
+    console.log("...");
+}
